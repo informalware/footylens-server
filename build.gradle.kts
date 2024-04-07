4ac06c5db5131b3dc5ffeef7d0c8bb33a9dd5bab
OJarrisonn_ <j.h.m.t.v.10@gmail.com>
2024-04-07 16:29:29 -0300

    Instalados novos plugins para serialização

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.9.23"
     id("io.ktor.plugin") version "2.3.9"
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 group = "com.informalware"
@@ -28,6 +29,8 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
