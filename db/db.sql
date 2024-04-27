2a5ed91dfe237a8c3f2bafe6017eb2fe731be896
OJarrisonn_ <j.h.m.t.v.10@gmail.com>
2024-04-27 16:25:22 -0300

    Fix: Tabelas renomeadas para evitar conflitos de palavras-chave
    Tabelas reordenadas

--- db/db.sql
+++ db/db.sql
@@ -1,14 +1,4 @@
-CREATE TABLE UserSession (
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    start_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    end_time TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES User(id)
-);
-
-
-
-CREATE TABLE User (
+CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -17,56 +7,64 @@ CREATE TABLE User (
     bio TEXT
 );
 
+CREATE TABLE UserSessions (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    start_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    end_time TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
 CREATE TABLE UserFollows (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     follower_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (follower_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (follower_id) REFERENCES Users(id)
 );
 
-CREATE TABLE TeamFollow (
-    id SERIAL PRIMARY KEY,
-    team_id INT NOT NULL,
-    follower_id INT NOT NULL,
-    FOREIGN KEY (team_id) REFERENCES team(id),
-    FOREIGN KEY (follower_id) REFERENCES user(id)
-);
-
-CREATE TABLE Review (
-    id SERIAL PRIMARY KEY,
-    match_id INT NOT NULL
-    user_id INT NOT NULL,
-    rating INT NOT NULL CHECK (rating >= 0 AND rating <= 4),
-    review TEXT,
-    creation_date DATE NOT NULL,
-    modified_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
-);
-
-CREATE TABLE Team (
+CREATE TABLE Teams (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     league VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Match (
+CREATE TABLE TeamFollows (
+    id SERIAL PRIMARY KEY,
+    team_id INT NOT NULL,
+    follower_id INT NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES Teams(id),
+    FOREIGN KEY (follower_id) REFERENCES Teams(id)
+);
+
+CREATE TABLE Matches (
     id SERIAL PRIMARY KEY,
     home INT NOT NULL,
     visitor INT NOT NULL,
     h_scoreboard INT NOT NULL,
     v_scoreboard INT NOT NULL,
-    FOREIGN KEY (home) REFERENCES team(id),
-    FOREIGN KEY (visitor) REFERENCES team(id)
+    FOREIGN KEY (home) REFERENCES Teams(id),
+    FOREIGN KEY (visitor) REFERENCES Teams(id)
 );
 
-CREATE TABLE Commentary (
+CREATE TABLE Reviews (
+    id SERIAL PRIMARY KEY,
+    match_id INT NOT NULL,
+    user_id INT NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 0 AND rating <= 4),
+    review TEXT,
+    creation_date DATE NOT NULL,
+    modified_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+CREATE TABLE Commentaries (
     id SERIAL PRIMARY KEY,
     review_id INT NOT NULL,
     user_id INT NOT NULL,
     commentary TEXT NOT NULL,
     creation_date DATE NOT NULL,
     modified_date DATE NOT NULL,
-    FOREIGN KEY (review_id) REFERENCES review(id),
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (review_id) REFERENCES Reviews(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
