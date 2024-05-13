e22d3e11bc72bd89653dc62a7057c6c2bf9a31ee
OJarrisonn_ <j.h.m.t.v.10@gmail.com>
2024-05-12 21:56:37 -0300

    Mocks follows Fixes some routes

--- db/mocks.sql
+++ db/mocks.sql
@@ -1,3 +1,7 @@
+DELETE FROM Reviews;
+DELETE FROM Commentaries;
+DELETE FROM Userfollows;
+DELETE FROM Teamfollows;
 DELETE FROM Users;
 DELETE FROM Teams;
 DELETE FROM Matches;
@@ -254,3 +258,77 @@ VALUES
 (16, 3, 'Dominant display by United, they’re showing real contender qualities.', '2021-10-19', '2021-10-19'),
 (16, 4, 'United are laying down markers for others this season.', '2021-10-19', '2021-10-19'),
 (16, 5, 'Another win under United’s belt, they are unstoppable.', '2021-10-19', '2021-10-19');
+
+INSERT INTO Userfollows (user_id, follower_id)
+VALUES
+(1, 2),
+(3, 4),
+(5, 6),
+(7, 8),
+(9, 10),
+(11, 12),
+(2, 1),
+(4, 3),
+(6, 5),
+(8, 7),
+(10, 9),
+(12, 11),
+(1, 3),
+(2, 4),
+(3, 5),
+(4, 6),
+(5, 7),
+(6, 8),
+(7, 9),
+(8, 10);
+
+INSERT INTO Teamfollows (team_id, follower_id)
+VALUES
+(1, 2),
+(3, 4),
+(5, 6),
+(7, 8),
+(9, 10),
+(11, 12),
+(13, 1),
+(2, 3),
+(4, 5),
+(6, 7),
+(8, 9),
+(10, 11),
+(20, 12),
+(1, 3),
+(2, 4),
+(3, 5),
+(4, 6),
+(5, 7),
+(6, 8),
+(7, 9),
+(8, 10),
+(9, 11),
+(10, 12),
+(11, 1),
+(12, 2),
+(2, 1),
+(4, 3),
+(6, 5),
+(8, 7),
+(10, 9),
+(12, 11),
+(1, 4),
+(3, 6),
+(5, 8),
+(7, 10),
+(9, 12),
+(13, 2),
+(1, 5),
+(2, 6),
+(3, 7),
+(4, 8),
+(5, 9),
+(6, 10),
+(7, 11),
+(8, 12),
+(9, 7),
+(10, 1),
+(11, 2);
